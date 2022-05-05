bb82c522e98d1a899e8028518ffef814326a2365
Hoon0506 <95264218+Hoon0506@users.noreply.github.com>
2022-05-05 22:46:40 +0900

    Revert "Hongki"

--- haroo/src/haroo/mapper/EmployeeMapper.java
+++
@@ -1,8 +0,0 @@
-package haroo.mapper;
-
-import haroo.domain.EmployeeVO;
-
-public interface EmployeeMapper {
-    EmployeeVO loginEmployee(EmployeeVO employeeVO);
-    int updateEmployee(EmployeeVO employeeVO);
-}
--- haroo/src/haroo/mapper/TaskMapper.java
+++
@@ -1,4 +0,0 @@
-package haroo.mapper;
-
-public interface TaskMapper {
-}
